6965a4e90d20bfabc7d4e823c1c035c259b94113
Baojiang Yang <baojiangyang@baojiangs-mbp.lan>
2021-09-06 15:54:53 -0400

    migrate bert code from wfh project

---
+++ src/model/bert_data_preprocess.py
@@ -0,0 +1,74 @@
+# ETL to construct task specific training dataset to fine-tune BERT
+# Goal: collect all labled data from SemEval2018 in the text-label format
+import numpy as np
+import pandas as pd
+import os
+
+import sys
+
+label_name = sys.argv[1]
+label_names = ['anger', 'anticipation','disgust','fear','joy','love','optimism','pessimism','sadness','surprise','trust']
+label_index = label_names.index(label_name) + 2
+print(label_index)
+
+input_dir = "~/Documents/twitter/data/affect_data/SemEval2018-Task1-all-data/English/E-c"
+
+train_name = "2018-E-c-En-train.txt"
+dev_name = "2018-E-c-En-dev.txt"
+test_name = "2018-E-c-En-test-gold.txt"
+
+train_file = os.path.join(input_dir, train_name)
+dev_file = os.path.join(input_dir, dev_name)
+test_file = os.path.join(input_dir, test_name)
+data_output_file = "~/Documents/twitter/data/affect_data/e_c_{}.csv".format(label_name)
+
+train = pd.read_csv(train_file, header=None, sep="\t",encoding='latin')
+dev = pd.read_csv(dev_file, header=None, sep="\t",encoding='latin')
+test = pd.read_csv(test_file, header=None, sep="\t",encoding='latin')
+
+train = train.loc[1:]
+dev = dev.loc[1:]
+test = test.loc[1:]
+
+df = train.append([dev, test])
+print(df.head(5))
+
+label_text = df[[1, label_index]]
+#label_text[3] = label_text[3].apply(lambda x: x[0])
+
+# Convert labels to range 0-1
+#label_text[0] = label_text[0].apply(lambda x: 0 if x == 0 else 1)
+
+# Assign proper column names to labels
+label_text.columns = ['text','label']
+
+# Assign proper column names to labels
+label_text.head()
+
+import re
+
+hashtags = re.compile(r"^#\S+|\s#\S+")
+mentions = re.compile(r"^@\S+|\s@\S+")
+urls = re.compile(r"https?://\S+")
+
+def process_text(text):
+  text = hashtags.sub(' hashtag', text)
+  text = mentions.sub(' entity', text)
+  return text.strip().lower()
+
+def match_expr(pattern, string):
+  return not pattern.search(string) == None
+
+def get_data_wo_urls(dataset):
+    link_with_urls = dataset.text.apply(lambda x: match_expr(urls, x))
+    return dataset[[not e for e in link_with_urls]]
+
+
+label_text.text = label_text.text.apply(process_text)
+
+
+print(label_text.head())
+print(label_text.shape)
+
+
+label_text.to_csv(data_output_file, index = False)
---
+++ src/model/bert_model_training.py
@@ -0,0 +1,263 @@
+# ENVIRONMENT SETUP
+import tensorflow as tf
+
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+  raise SystemError('GPU device not found')
+print('Found GPU at: {}'.format(device_name))
+
+
+import re
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from pytorch_pretrained_bert import BertTokenizer, BertConfig
+from pytorch_pretrained_bert import BertAdam, BertForSequenceClassification
+from tqdm import tqdm, trange
+import pandas as pd
+import io
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+#% matplotlib inline
+from pytorch_pretrained_bert import WEIGHTS_NAME, CONFIG_NAME
+from sklearn.metrics import matthews_corrcoef, confusion_matrix
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_gpu = torch.cuda.device_count()
+torch.cuda.get_device_name(0)
+
+# READING PARAMETERS FROM COMMAND LINE
+import sys
+
+label_name = sys.argv[1]
+learning_rate = float(sys.argv[2])
+n_epoch = int(sys.argv[3])
+
+print("label name: " + str(label_name))
+print("learning rate: " + str(learning_rate))
+print("number of epoches: " + str(n_epoch))
+
+# READING TRAINING DATA TO FINE-TUNE BERT
+# Note: please use data after "upsampling" so that the binary labels have 50/50 of 1/0s.
+data_output_file = "~/Documents/twitter/data/affect_data/e_c_{}.csv".format(label_name)
+label_text = pd.read_csv(data_output_file)
+
+print(label_text.head( 10))
+
+# Pad sentences with start and end tokens used for BERT
+sentences = label_text.text.values
+sentences = ["[CLS] " + sentence + " [SEP]" for sentence in sentences]
+
+print("enter tokenization")
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+tokenized_texts = [tokenizer.tokenize(sent) for sent in sentences]
+print ("Tokenize the first sentence:")
+print (tokenized_texts[0])
+
+labels = label_text.label.values
+type(labels)
+type(labels[0])
+
+# SEQUENCE PADDING
+# Maximum sentence length set to 256
+MAX_LEN = 256
+# Pad input tokens
+input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts],
+                          maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+# Use the BERT tokenizer to convert the tokens to corresponding index numbers in the BERT vocabulary
+input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
+#Pad the sequences
+input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+
+print(input_ids[1])
+
+print("finished sequence padding")
+
+# Create attention masks
+attention_masks = []
+
+# Create a mask of 1s for each token followed by 0s for padding
+for seq in input_ids:
+  seq_mask = [float(i>0) for i in seq]
+  attention_masks.append(seq_mask)
+
+# Use train_test_split to split our data into train and validation sets for training
+
+train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids, labels,
+                                                            random_state=2018, test_size=0.2)
+train_masks, validation_masks, _, _ = train_test_split(attention_masks, input_ids,
+                                             random_state=2018, test_size=0.2)
+
+print("Convert all of our data into torch tensors, the required datatype for our model")
+
+train_inputs = torch.tensor(train_inputs)
+validation_inputs = torch.tensor(validation_inputs)
+train_labels = torch.tensor(train_labels)
+validation_labels = torch.tensor(validation_labels)
+train_masks = torch.tensor(train_masks)
+validation_masks = torch.tensor(validation_masks)
+
+# Select a batch size for training. For fine-tuning BERT on a specific task - based on the GPU environment
+print("set batch size")
+batch_size = 32
+
+# Create an iterator with torch DataLoader
+train_data = TensorDataset(train_inputs, train_masks, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
+
+print("finished batch setup")
+
+
+# MODEL TRAINING
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
+model.cuda()
+
+print("finished loading bert-base-uncased")
+
+# Setup Adam optimizer parameters
+param_optimizer = list(model.named_parameters())
+no_decay = ['bias', 'gamma', 'beta']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.01},
+    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.0}
+]
+
+print("finished setting different weight decays for different layers of the model")
+
+optimizer = BertAdam(optimizer_grouped_parameters,
+                     lr=learning_rate,
+                     warmup=.1)
+
+print("finished setting BertAdam hyperparameters")
+
+# Function to calculate the accuracy of our predictions vs labels
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+def get_eval_report(labels, preds):
+    mcc = matthews_corrcoef(labels, preds)
+    tn, fp, fn, tp = confusion_matrix(labels, preds).ravel()
+    return {
+        "mcc": mcc,
+        "tp": tp,
+        "tn": tn,
+        "fp": fp,
+        "fn": fn
+    }
+
+def compute_metrics(labels, preds):
+    assert len(preds) == len(labels)
+    return get_eval_report(labels, preds)
+
+print(compute_metrics([1,1,1,1], [0,0,1,1]))
+
+# Store our loss and accuracy for plotting
+train_loss_set = []
+
+# Number of training epochs (recommend between 2 and 5)
+epochs = n_epoch
+
+# trange is a tqdm wrapper around the normal python range
+for _ in trange(epochs, desc="Epoch"):
+  # Training
+
+  # Set our model to training mode (as opposed to evaluation mode)
+  model.train()
+
+  # Tracking variables
+  tr_loss = 0
+  nb_tr_examples, nb_tr_steps = 0, 0
+
+  # Train the data for one epoch
+  for step, batch in enumerate(train_dataloader):
+    # Add batch to GPU
+    batch = tuple(t.to(device) for t in batch)
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+    # Clear out the gradients (by default they accumulate)
+    optimizer.zero_grad()
+    # Forward pass
+    loss = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
+    train_loss_set.append(loss.item())
+    # Backward pass
+    loss.backward()
+    # Update parameters and take a step using the computed gradient
+    optimizer.step()
+
+
+    # Update tracking variables
+    tr_loss += loss.item()
+    nb_tr_examples += b_input_ids.size(0)
+    nb_tr_steps += 1
+
+  print("Train loss: {}".format(tr_loss/nb_tr_steps))
+  print("Train loss: {}".format(tr_loss/nb_tr_steps))
+
+  # Validation
+
+  # Put model in evaluation mode to evaluate loss on the validation set
+  model.eval()
+
+  # Tracking variables
+  eval_loss, eval_accuracy = 0, 0
+  nb_eval_steps, nb_eval_examples = 0, 0
+
+  # Evaluate data for one epoch
+  for batch in validation_dataloader:
+    # Add batch to GPU
+    batch = tuple(t.to(device) for t in batch)
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+    # Telling the model not to compute or store gradients, saving memory and speeding up validation
+    with torch.no_grad():
+      # Forward pass, calculate logit predictions
+      logits = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+
+    # Move logits and labels to CPU
+    logits = logits.detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+
+    tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+
+    eval_accuracy += tmp_eval_accuracy
+    nb_eval_steps += 1
+
+  print("Validation Accuracy: {}".format(eval_accuracy/nb_eval_steps))
+  print("Validation Accuracy: {}".format(eval_accuracy/nb_eval_steps))
+
+
+
+from pytorch_pretrained_bert import WEIGHTS_NAME, CONFIG_NAME
+
+output_dir = "~/Documents/twitter/model/affect_model/{}/".format(label_name)
+
+# Step 1: Save a model, configuration and vocabulary that you have fine-tuned
+
+# If we have a distributed model, save only the encapsulated model
+# (it was wrapped in PyTorch DistributedDataParallel or DataParallel)
+model_to_save = model.module if hasattr(model, 'module') else model
+
+# If we save using the predefined names, we can load using `from_pretrained`
+output_model_file = os.path.join(output_dir, WEIGHTS_NAME)
+output_config_file = os.path.join(output_dir, CONFIG_NAME)
+
+torch.save(model_to_save.state_dict(), output_model_file)
+model_to_save.config.to_json_file(output_config_file)
+tokenizer.save_vocabulary(output_dir)
+
+# reference: https://www.tensorflow.org/official_models/fine_tuning_bert
+# reference: https://github.com/sidmahurkar/BERT-Twitter-US-Airline-Sentiment-Analysis/blob/master/Bert_airline_senti_analysis.ipynb
+# reference: https://mccormickml.com/2019/07/22/BERT-fine-tuning/#5-performance-on-test-set
